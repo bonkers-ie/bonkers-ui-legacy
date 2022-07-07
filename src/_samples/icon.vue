379bf866d65a8cd6f4414794f49c1462c05f7dfa
Andrew <andrii.fedotiuk@bonkers.ie>
2022-07-07 16:17:39 +0100

    update(ui-input): add sample of default input

---
+++ src/_samples/icon.vue
@@ -0,0 +1,19 @@
+<template>
+	<svg
+		:width="size"
+		:height="size"
+		viewBox="0 0 100 100"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
+	>
+		<path
+			d="M50 0L60.9065 23.6694L85.3553 14.6447L76.3306 39.0935L100 50L76.3306 60.9065L85.3553 85.3553L60.9065 76.3306L50 100L39.0935 76.3306L14.6447 85.3553L23.6694 60.9065L0 50L23.6694 39.0935L14.6447 14.6447L39.0935 23.6694L50 0Z"
+		/>
+	</svg>
+</template>
+
+<script lang="ts" setup>
+	defineProps<{
+		size: number
+	}>();
+</script>
---
+++ src/components/ui-input/ui-input.vue
@@ -0,0 +1,43 @@
+<template>
+	<div
+		class="ui-input grid rounded-md border border-secondary-alt-500 bg-white max-w-xs items-center p-xs gap-xs"
+		:class="[
+			kind === EInputTypes.PRIMARY && 'border-primary',
+			fullWidth && 'max-w-full',
+		]"
+	>
+		<div class="icon-wrapper">
+			<slot name="prefix-icon" />
+		</div>
+		<input
+			class="bg-transparent border-0 outline-0"
+			type="text"
+			:placeholder="placeholder"
+			:value="modelValue"
+			@input="$emit('update:modelValue', $event.target.value)"
+		>
+		<div class="icon-wrapper">
+			<slot name="postfix-icon" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	import { EInputTypes } from "./_typings";
+
+	defineProps<{
+		placeholder: string;
+		modelValue: string;
+		disabled?: boolean;
+		fullWidth?: boolean;
+		kind?: EInputTypes;
+	}>();
+
+	defineEmits(["update:modelValue"]);
+</script>
+
+<style scoped>
+	.ui-input {
+		grid-template-columns: auto 1fr auto;
+	}
+</style>
