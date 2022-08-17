3cf5191cfc185866c8fdb7a2990cc3bbfb06fd29
Andrew <andrii.fedotiuk@bonkers.ie>
2022-08-17 15:53:46 +0100

    update(ui-ber-rank): update bar rank props

--- src/components/ui-ber-rank/ui-ber-rank.vue
+++ src/components/ui-ber-rank/ui-ber-rank.vue
@@ -56,83 +56,11 @@
 <script lang="ts" setup>
 	import UiTypography, { ETextWeight, ETypographySizes, ETextTransform } from "../ui-typography";
 	import { EColors } from "../../_types/colors";
-
-	type Ran<T extends number> = number extends T ? number :_Range<T, []>;
-	type _Range<T extends number, R extends unknown[]> = R["length"] extends T ? R[number] : _Range<T, [R["length"], ...R]>;
-
-	type TPropNumber = Ran<typeof berRankDictionary.length>
-	type TPropString = `${TPropNumber}`;
+	import { berRankDictionary, type TBerPropNumber, type TBerPropString }from"./_types";
 
 	defineProps<{
-		rank: TPropNumber | TPropString | number; // from 0 to 15
+		rank: TBerPropNumber | TBerPropString | number; // from 0 to 15
 	}>();
-
-	const berRankDictionary = [
-		{
-			text: "N/A",
-			color: EColors.SECONDARY_ALT,
-		},
-		{
-			text: "G",
-			color: EColors.ERROR,
-		},
-		{
-			text: "F",
-			color: EColors.WARNING,
-		},
-		{
-			text: "E2",
-			color: EColors.WARNING_600,
-		},
-		{
-			text: "E1",
-			color: EColors.WARNING_600,
-		},
-		{
-			text: "D2",
-			color: EColors.WARNING_400,
-		},
-		{
-			text: "D1",
-			color: EColors.WARNING_400,
-		},
-		{
-			text: "C3",
-			color: EColors.PRIMARY_ALT,
-		},
-		{
-			text: "C2",
-			color: EColors.PRIMARY_ALT,
-		},
-		{
-			text: "C1",
-			color: EColors.PRIMARY_ALT,
-		},
-		{
-			text: "B3",
-			color: EColors.PRIMARY_ALT_600,
-		},
-		{
-			text: "B2",
-			color: EColors.PRIMARY_ALT_600,
-		},
-		{
-			text: "B1",
-			color: EColors.PRIMARY_ALT_600,
-		},
-		{
-			text: "A3",
-			color: EColors.PRIMARY_ALT_700,
-		},
-		{
-			text: "A2",
-			color: EColors.PRIMARY_ALT_700,
-		},
-		{
-			text: "A1",
-			color: EColors.PRIMARY_ALT_700,
-		},
-	] as const;
 </script>
 
 <style>
