31b081606e9b7bdcd0c5f39c9d2b0b3aa4bd64a3
Andrew <andrii.fedotiuk@bonkers.ie>
2022-09-15 15:32:58 +0100

    update(ui-select): fix styles

--- src/components/ui-select/ui-select.stories.ts
+++ src/components/ui-select/ui-select.stories.ts
@@ -26,9 +26,8 @@ const Template: Story<TComponentProps> = (args) => ({
 		const valueModel = ref(list[0]);
 		return { args, valueModel, list };
 	},
-	template: `
+	template: /*html*/`
 		<ui-select v-bind="args" :list="list" v-model:value="valueModel" heading="heading" subLabel="subLabel">
-			{{ args.slot }}
 			<template #postfix-icon>
 				<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path d="M13.25 6.8125L8.5 11.2812C8.34375 11.4375 8.15625 11.5 8 11.5C7.8125 11.5 7.625 11.4375 7.46875 11.3125L2.71875 6.8125C2.40625 6.53125 2.40625 6.0625 2.6875 5.75C2.96875 5.4375 3.4375 5.4375 3.75 5.71875L8 9.71875L12.2188 5.71875C12.5312 5.4375 13 5.4375 13.2812 5.75C13.5625 6.0625 13.5625 6.53125 13.25 6.8125Z" fill="currentColor"/>
