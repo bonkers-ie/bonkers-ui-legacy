ae1e2e1d24b080adf933fbf996f2f5cd123570ae
Sean Lucey <sean.lucey@bonkers.ie>
2022-10-24 17:24:26 +0100

    Added modal animation, modified props & optional secondary button

--- src/components/ui-modal/ui-modal.stories.ts
+++ src/components/ui-modal/ui-modal.stories.ts
@@ -2,9 +2,8 @@ import UiModal from "./ui-modal.vue";
 import type { Story } from "@storybook/vue3";
 import { EModalTypes } from "./_types";
 import { ESize } from "../../_types/sizing";
-import UiButton, { EButtonTypes } from "../ui-button";
+import UiButton, { EButtonSizes, EButtonTypes } from "../ui-button";
 import { reactive, toRefs } from "vue";
-
 
 export default {
 	title: "Components/ui-modal",
@@ -15,10 +14,6 @@ export default {
 			options: Object.values(EModalTypes),
 			description: "The modal kinds",
 		},
-		modalVisible: {
-			control: { type: "boolean" },
-			description: "Control Modal Visibility",
-		},
 		title: {
 			control: { type: "text" },
 			description: "The modal title text",
@@ -27,48 +22,78 @@ export default {
 			control: { type: "text" },
 			description: "The modal description text",
 		},
+		icon: {
+			control: { type: "select" },
+			options: ["circle-check", "face-smile" ]
+		},
+		iconSize: {
+			control: { type: "select" },
+			options: Object.values(ESize),
+			description: "Icon size for modal",
+		},
+		modalSize: {
+			control: { type: "select" },
+			options: Object.values(ESize),
+			description: "The modal kinds",
+		},
+		modalVisible: {
+			control: { type: "boolean" },
+			description: "Control Modal Visibility",
+		},
+		secondaryBtn: {
+			control: { type: "select" },
+			options: [null,...Object.values(EButtonTypes)],
+			description: "Optionally add a secondary button to the modal, specifying button type",
+		},
 
 	},
 	args: {
 		title: "Password Updated",
 		description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
 		kind: EModalTypes.PRIMARY,
-		icon: "",
-		iconSize: ESize.LG,
-		modalSize: ESize.MD,
+		secondaryBtn: EButtonTypes.SECONDARY_OVERLAY ,
+		icon: "circle-check",
+		iconSize: ESize.XL,
+		modalSize: ESize.SM,
 		modalVisible: false,
 	}
 };
 
 type TComponentProps = InstanceType<typeof UiModal>["$props"];
 
-const Template: Story<TCbomponentProps> = (args) => ({
+const Template: Story<TComponentProps> = (args) => ({
 	components: { UiModal, UiButton },
 	setup() {
 
 		const state = reactive({
 			modalVisible: false
-		})
+		});
 		const showModal = () => {
-			console.log("Showing Modal");
 			state.modalVisible = true;
 		};
 		const closeModal = () => {
-			console.log("Closing Modal");
 			state.modalVisible = false;
-		}
+		};
+		const handlePrimaryClick = () => {
+			closeModal();
+		};
+		const handleSecondaryClick = () => {
+			closeModal();
+		};
 
-		return { args, showModal, closeModal, ...toRefs(state) };
+		return { args, showModal, closeModal, handlePrimaryClick, handleSecondaryClick, ...toRefs(state) };
 	},
 	template:`
 			<div>
 				<ui-modal
 					:kind="args.kind"
-					:icon="['far', 'circle-check']"
+					:icon="['far', args.icon]"
 					:iconSize="args.iconSize"
 					:modalVisible.sync="modalVisible"
 					:modalSize="args.modalSize"
-					@closeModal="closeModal"
+					:secondaryBtn="args.secondaryBtn"
+					@modal-primary-click="handlePrimaryClick"
+					@modal-secondary-click="handleSecondaryClick"
 				>
 					<template v-slot:title>
 						{{args.title}}
@@ -78,11 +103,11 @@ const Template: Story<TCbomponentProps> = (args) => ({
 					</template>
 				</ui-modal>
 
-				<div class="absolute bottom-4">
-				<ui-button
-				@click="showModal">
-				Toggle Modal
-				</ui-button>
+				<div class="absolute" style="top: calc(50vh - 2rem); left: calc(50vw - 4rem)">
+					<ui-button
+					@click="showModal">
+					Toggle Modal
+					</ui-button>
 				</div>
 			</div>
 			`
--- src/components/ui-modal/ui-modal.vue
+++ src/components/ui-modal/ui-modal.vue
@@ -1,27 +1,36 @@
 <template>
-	<div  class="overlay">
-		<div v-if="modalVisible"
+	<transition
+		name="ui-modal"
+		mode="out-in"
+	>
+		<div
+			v-if="modalVisible"
 			:class="[
-						'flex flex-col justify-center items-center rounded-3xl shadow-md py-md px-sm bg-white',
-					]"
-			>
+				'ui-modal absolute grid grid-rows-6 grid-flow-col gap-4 place-items-center rounded-3xl shadow-md py-md px-sm bg-white',
+				modalSize === ESize.XXS && 'h-[12rem] w-[12rem]',
+				modalSize === ESize.XS && 'h-[18rem] w-[18rem]',
+				modalSize === ESize.SM && 'h-[24rem] w-[24rem]',
+				modalSize === ESize.MD && 'h-[24rem] w-[36rem]',
+				modalSize === ESize.LG && 'h-[24rem] w-[48rem]',
+				modalSize === ESize.XL && 'h-[24rem] w-[60rem]',
+				modalSize === ESize.XXL && 'h-[24rem] w-[72rem]',
+			]"
+		>
 			<ui-icon
 				:icon-name="icon"
 				:size="iconSize"
 				:class="[
-					'mb-md',
-					(!kind || kind === EModalTypes.PRIMARY) && 'text-primary',
+					'row-start-1 row-span-2',
+					(!kind || kind === EModalTypes.PRIMARY) &&'text-primary',
 					kind === EModalTypes.WARNING && 'text-warning',
 					kind === EModalTypes.ERROR && 'text-error',
 					kind === EModalTypes.SECONDARY && 'text-secondary',
-
 				]"
 			/>
 			<ui-typography
 				v-if="slots.title"
 				:weight="ETextWeight.BOLD"
 				:align="ETextAlign.CENTER"
-				class="pb-md"
 			>
 				<slot name="title" />
 			</ui-typography>
@@ -29,43 +38,117 @@
 				v-if="slots.description"
 				:weight="ETextWeight.REGULAR"
 				:align="ETextAlign.CENTER"
-				class="pb-md"
 			>
 				<slot name="description" />
 			</ui-typography>
-			<ui-button full-width @click="modalVisibility">
-				OK
-			</ui-button>
+			<div class="grid grid-cols-7 gap-sm grid-flow-row row-start-6 w-full">
+				<ui-button
+					v-if="secondaryBtn"
+					:kind="secondaryBtn"
+					:class="[
+						(modalSize === ESize.XS || modalSize === ESize.SM || modalSize === ESize.MD) && 'col-end-5 col-span-1 px-lg',
+						(modalSize === ESize.LG || modalSize === ESize.XL || modalSize === ESize.XXL) && 'col-end-6'
+					]"
+					@click="onSecondaryClick"
+				>
+					Cancel
+				</ui-button>
+				<ui-button
+					:full-width="!secondaryBtn"
+					:class="[(secondaryBtn && modalSize === ESize.LG || modalSize === ESize.XL|| modalSize === ESize.XXL) ? 'col-end-8 col-span-2' : 'col-end-8 col-span-2'
+					]"
+					@click="onPrimaryClick"
+				>
+					OK
+				</ui-button>
+			</div>
 		</div>
-	</div>
-
+	</transition>
+	<transition
+		name="ui-backdrop"
+		mode="out-in"
+	>
+		<div
+			v-if="modalVisible"
+			class="ui-modal-backdrop absolute overflow-y-auto h-full w-full bg-black opacity-60"
+		/>
+	</transition>
 </template>
 <script lang="ts" setup>
 	import UiIcon, { type TIconName } from "../ui-icon";
-	import UiButton from "../ui-button"
+	import UiButton, { EButtonTypes } from "../ui-button";
 	import UiTypography, { ETextWeight, ETextAlign } from "../ui-typography";
 	import { ESize } from "../../_types/sizing";
 	import { EModalTypes } from "./_types";
-	import { useSlots, computed } from "vue";
+	import { useSlots } from "vue";
 
 	const slots = useSlots();
 
-		defineProps<{
-			modalVisible: boolean
-			icon: TIconName
-			iconSize: ESize
-			modalSize: ESize
-			kind?: EModalTypes
-		}>();
-
-	const emit = defineEmits<{
-		(e: "closeModal", state: boolean): void
+	defineProps<{
+		modalVisible: boolean
+		secondaryBtn?: {
+			type: EButtonTypes,
+			default: EButtonTypes.SECONDARY_OVERLAY
+		},
+		icon: TIconName
+		iconSize: ESize
+		modalSize?: ESize
+		kind?: {
+			type: EModalTypes,
+			default: EModalTypes.PRIMARY
+		},
 	}>();
 
-	const modalVisibility = () => {
-		emit("closeModal", false)
-	};
+	const emit = defineEmits<{
+		(e: "modal-secondary-click", state: boolean): void
+		(e: "modal-primary-click", state: boolean): void
+	}>();
 
+	const onSecondaryClick = () =>{
+		emit("modal-secondary-click", false);
+	};
+	const onPrimaryClick = () => {
+		emit("modal-primary-click", false);
+	};
 </script>
-<style lang="scss" scoped>
+<style lang="pcss" scoped>
+.ui-modal{
+	z-index: 9999;
+	transform: none;
+	opacity: 1;
+	top:0;
+	right:0;
+	bottom:0;
+	left: 0;
+	margin: auto;
+}
+.ui-modal-backdrop{
+	display: flex;
+	z-index: 9998;
+}
+	.ui-backdrop-enter-active,
+.ui-backdrop-leave-active {
+  transition: opacity .15s linear;
+}
+
+.ui-backdrop-enter-from,
+.ui-backdrop-leave-to {
+  opacity: 0;
+}
+
+.ui-backdrop-leave-to{
+	transition-delay: 0.1s;
+}
+
+.ui-modal-enter-active,
+.ui-modal-leave-active {
+  transition: transform 0.2s cubic-bezier(0.5, 0, 0.5, 1), opacity 0.2s linear;
+  transition-delay: 0.1s;
+}
+
+.ui-modal-enter-from,
+.ui-modal-leave-to {
+  opacity: 0;
+  transform: translateY(20%);
+}
 </style>
