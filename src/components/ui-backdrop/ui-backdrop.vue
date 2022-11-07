7b42e2a261b67a3dc9ebaebd0750ea79e41e4b16
Sean Lucey <sean.lucey@bonkers.ie>
2022-11-07 11:56:09 +0000

    add(ui-backdrop)

---
+++ src/components/ui-backdrop/ui-backdrop.vue
@@ -0,0 +1,35 @@
+<template>
+	<div
+		class="ui-backdrop"
+	>
+		<transition
+			name="ui-backdrop"
+			enter-active-class="transition-opacity"
+			enter-from-class="opacity-0"
+			leave-active-class="transition-opacity"
+			leave-to-class="opacity-0 delay-400"
+		>
+			<div
+				v-if="visible"
+				class="
+				absolute
+				overflow-y-auto
+				h-full
+				w-full
+				bg-black
+				opacity-60
+				flex
+				z-0
+				"
+			/>
+		</transition>
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+	defineProps<{
+		visible: boolean;
+	}>();
+
+</script>
