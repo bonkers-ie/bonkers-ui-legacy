4380b34dc6c2c04d90ed653940061b386ee3e6b1
Andrew <andrii.fedotiuk@bonkers.ie>
2022-12-02 15:05:15 +0000

    update(ui-list-items): update icon classes

--- src/components/ui-list-item/ui-list-item.vue
+++ src/components/ui-list-item/ui-list-item.vue
@@ -1,35 +1,34 @@
 
 <template>
 	<li
-		class="ui-list-item flex gap-xs relative group"
+		class="ui-list-item grid grid-flow-col justify-start gap-xs relative group"
 		:class="[
 			size === EListItemSizes.DEFAULT && 'pb-md',
 			size === EListItemSizes.COMPACT && 'pb-xs'
 		]"
 	>
 		<div
-			v-if="kind===EListItemTypes.PROGRESS"
+			v-if="kind === EListItemTypes.PROGRESS"
 			class="ui-list-item__line bg-primary-300 h-full absolute w-xxs left-xs -translate-x-2/4 top-sm group-last:hidden"
 		/>
 
-		<span>
-			<slot name="icon">
-				<ui-icon
-					v-if="icon"
-					class="bg-white"
-					:icon-name="icon"
-					:size="ESize.SM"
-				/>
-			</slot>
-		</span>
+		<slot name="icon">
+			<ui-icon
+				v-if="icon"
+				class="bg-white"
+				:icon-name="icon"
+				:size="ESize.SM"
+			/>
+		</slot>
 
-		<span class="leading-4">
+		<span>
 			<ui-typography
 				v-if="title"
 				:weight="ETextWeight.SEMI_BOLD"
 				line-height
 			>
 				{{ title }}
+
 			</ui-typography>
 			<slot />
 		</span>
