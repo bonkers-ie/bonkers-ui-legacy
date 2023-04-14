6aeb1f8562ded519167897fb43297b093590b2a2
Andrew <andrii.fedotiuk@bonkers.ie>
2023-04-14 16:09:03 +0100

    add(ui-snackbar): add new component

---
+++ src/components/ui-snackbar/ui-snackbar.vue
@@ -0,0 +1,42 @@
+<template>
+	<div
+		class="ui-snackbar flex shadow-md rounded-lg w-full p-sm justify-between"
+		:class="[
+			kind === ESnackbarTypes.DEFAULT && 'bg-white',
+			kind === ESnackbarTypes.PRIMARY && 'bg-primary text-white',
+			kind === ESnackbarTypes.WARNING && 'bg-warning text-white',
+			kind === ESnackbarTypes.ERROR && 'bg-error text-white',
+			kind === ESnackbarTypes.SECONDARY && 'bg-secondary text-white',
+		]"
+	>
+		<slot>
+			<ui-typography
+				:size="ETypographySizes.SM"
+				class=""
+			>
+				<slot name="titleSlot">
+					{{ title }}
+				</slot>
+			</ui-typography>
+
+			<ui-typography
+				:size="ETypographySizes.XS"
+				class=""
+			>
+				<slot name="articleSlot" />
+			</ui-typography>
+		</slot>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	import UiTypography, { ETypographySizes } from "../ui-typography";
+	import { ESnackbarTypes } from "./_types";
+
+	withDefaults(defineProps<{
+		title: string
+		kind?: ESnackbarTypes
+	}>(), {
+		kind: ESnackbarTypes.DEFAULT
+	});
+</script>
