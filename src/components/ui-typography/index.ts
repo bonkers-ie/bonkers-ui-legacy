411f526b006b26311f773617d8696f1764b8259a
Andrew <andrii.fedotiuk@bonkers.ie>
2023-11-09 11:42:13 +0000

    update(types): update type comments

--- src/components/ui-typography/index.ts
+++ src/components/ui-typography/index.ts
@@ -1,3 +1,8 @@
-export { default } from "./ui-typography.vue";
+import { withInstall } from "../../_types/helper";
+import Typography from "./ui-typography.vue";
+
 export { ETypographySizes, ETextWeight, ETextAlign, ETextTransform } from "./_typings";
 export { EColors } from "../../_types/colors";
+
+export const UiTypography = withInstall(Typography);
+export default UiTypography;
